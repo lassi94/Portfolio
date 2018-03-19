9aee00bf26751f825d45a5dfbc67a2126f4a5aef
Lassi Korhonen <lasiko87@wippies.com>
2018-03-19 23:33:11 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <div class="container main-content">
+    <nav class="navbar" role="navigation">
+      <div class="container"> 
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">LK</router-link>
+
+        <div class="navbar-burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+        <div class="navbar-menu">
+          <div class="navbar-start">
+           
+          </div>
+          <div class="navbar-end">
+            <router-link to="/" class="navbar-item">Home</router-link>
+            <router-link to="/about" class="navbar-item">About</router-link>
+            <router-link to="/blog" class="navbar-item">Blog</router-link>
+            <a class="navbar-item">
+              <router-link to="/contact" class="button is-oulined">
+              <span id="contact">Contact me</span>
+              </router-link>
+            </a>
+          </div>
+      </div>
+      </div>
+    </nav>
+
+    <transition name="fade" mode="out-in">
+        <router-view />
+    </transition>
+
+    </div>
+    <!--
+    <div class="container">
+        <div class="inner">
+          <nav>
+            <router-link to="/" id="logo">LK</router-link>
+            
+            <ul>
+              <li><router-link to="/">Home</router-link></li>
+              <li><router-link to="/about">About</router-link></li>
+            </ul>
+
+          </nav>
+
+          <transition name="fade" mode="out-in">
+            <router-view />
+          </transition>
+
+        </div>
+    </div>-->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+
+@import '../node_modules/bulma/bulma.sass'
+/*@import 'mq.sass'*/
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700')
+
+#app 
+  font-family: 'Montserrat'
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
+.main-content
+  margin: 50px auto
+  background-color: #fff
+
+//body
+  //background-color: #F1F0F0
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,73 @@
+<template>
+
+  <div class="home main">
+
+    <section class="section">
+
+      <div class="container">
+      <div class="columns">
+        <div class="column is-half pd">
+            <h1 class="title">PASSIONATE SOFTWARE DEVELOPER</h1>
+            <p class="subtitle">{{ subtext }}</p>
+            <h2 class="title">Find me</h2>
+            <hr align="center">
+          
+            <a href="https://github.com">
+              <span class="icon">
+                <i class="fab fa-github"></i>
+              </span>
+            </a>
+
+            <a href="https://linkedin.com">
+              <span class="icon">
+                <i class="fab fa-linkedin"></i>
+              </span>
+          </a>
+        </div>
+        <div class="column is-half pd1">
+          <img src="" alt="">
+        </div>
+      </div>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+        subtext: 'Hi, Im Lassi Korhonen. I am an ambitious and passionate software developer who enjoys programming and learning new technologies. I have keen interest also in UI design and cyber security.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+h1, h2 
+  font-weight: normal
+a 
+  color: #42b983
+
+h1.title
+    margin-top: 20px
+    margin-bottom: 70px
+    text-transform: uppercase
+    text-decoration: none
+    text-align: left
+
+p.subtitle
+  margin-bottom: 70px
+  text-align: left
+
+hr
+  width: 50%
+  margin: 0 auto
+  margin-top: -15px
+  
+  
+
+</style>
